41921a50c0e53a246be44175a65e8c14e6c40010
ErtiDesu <erti.rakipaj@gmail.com>
2022-11-24 18:16:57 +0100

    Initial Commit

---
+++ src/app/component/userdetail/userdetail.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { User } from 'src/app/interface/user.interface';
+import { UserService } from 'src/app/service/user.service';
+
+@Component({
+  selector: 'app-userdetail',
+  templateUrl: './userdetail.component.html',
+  styleUrls: ['./userdetail.component.css']
+})
+export class UserdetailComponent implements OnInit { 
+  user: User
+  mode: 'edit' | 'locked' = 'locked';
+  buttonText: 'Save Changes' | 'Edit' = 'Edit';
+
+  constructor(private activatedRoute: ActivatedRoute, private userService: UserService) {}
+
+  ngOnInit(): void {
+      this.user = (<User>(this.activatedRoute.snapshot.data['resolvedResponse'].results[0]));
+      console.log(this.user);
+      // // this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
+      // //   console.log('User ID: ', params.get('uuid')!);
+      // //     this.userService.getUser(params.get('uuid')!).subscribe(
+      // //       (response:any) => {
+      // //         console.log(response);
+      // //         this.response = response;
+      // //       }
+      // //     );
+      // });
+  }
+
+  changeMode(mode?: 'edit' | 'locked'):void {
+      console.log(mode);
+      this.mode = this.mode ==='locked' ? 'edit' : 'locked';
+      this.buttonText = this.buttonText === 'Edit' ? 'Save Changes' : 'Edit';
+      if(mode === 'edit') {
+        //Logic to update the user
+        console.log('Updating user on the back end');
+      }
+  }
+
+}
